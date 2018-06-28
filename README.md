60b679e3e79bae9148bf71a534b3f0d52df3214e
Zoe Goss <zg811@imperial.ac.uk>
2018-06-28 16:50:20 +0100

    Fixed the conflict

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 ##
 
 ###
+
+Project overview
+-----------------
+
+This mini project aims to develop an understanding of the spatial dependence of weather predicatability in the UK region. The idea is to decompose the area of the UK into "superpixels", each containing a relatively homogeneous area with respect to some meteorological property. Statistical techniques are then applied on the set of superpixels (superpixel-mean MOGREPS data) in order to understand what determines unpredictability in and around the UK. 
--- scatter_plotting.py
+++ scatter_plotting.py
@@ -9,6 +9,10 @@ import matplotlib.gridspec as gs
 
 def scatterplot(lists,var_names):
 
+	"""
+	Function to create an array of scatter plots.
+	"""
+
 	# lists = [mean or variance, flow property, segment number]
 
 	Nv, Np, Ns = np.shape(lists) # Nv = 2 (mean or variance), Np = no of flow properties, Ns = no of segments
@@ -25,10 +29,10 @@ def scatterplot(lists,var_names):
 	# Define width ratio of each subplot
 	grsp = gs.GridSpec(Np,Np)
 
-	for row in range(0,Np):
+	for row in range(Np):
 		x = lists[0,row,:]
 		row_ = Np - 1 - row
-		for column in range(0,Np):
+		for column in range(Np):
 			y = lists[1,column,:]
 
 			plt.subplot(grsp[row_,column])
@@ -36,17 +40,66 @@ def scatterplot(lists,var_names):
 
 			plt.xticks(np.linspace(varmin[0,column],varmax[0,column],5),fontsize=0)
 			plt.yticks(np.linspace(varmin[1,row],varmax[1,row],5),fontsize=0)
-	
-			#dx = (varmax[0,column] - varmin[0,column]) / 5
-			#dy = (varmax[1,row] - varmin[1,row]) / 5
-			#plt.xlim(varmin[0,column]-dx,varmax[0,column]+dx)
-			#plt.xlim(varmin[1,row]-dx,varmax[1,row]+dy)
 
 			if row_ == Np-1:
 				plt.xlabel(var_names[column])
 			if column == 0:
 				plt.ylabel(var_names[row])
+		
+			#plt.xlabel('MEAN',fontsize=16)
+			#plt.ylabel('VAR',rotation=90,fontsize=16)
 
 	plt.tight_layout(pad=0.3, w_pad=0.2, h_pad=0.2)
-	plt.savefig('scatter.png')
+	#plt.savefig('scatter.png')
 	plt.show()
+
+
+#==================================================
+
+def corr(lists,var_names):
+
+	"""
+	Function to calculate and plot the correlation between means and variances of a set of variables.
+	"""
+
+	Nv, Np, Ns = np.shape(lists)
+	print(Nv)
+	# Find correlation coefficients.
+	corr = np.zeros((Np,Np))
+	for row in range(Np):
+		for column in range(Np):
+			corr[row,column] = np.corrcoef(lists[0,row,:],lists[1,column,:])[0,1]
+	
+	fig, ax = plt.subplots()
+	fig.subplots_adjust(bottom=0.25,left=0.25) # Make room for labels
+
+	heatmap = ax.pcolor(corr,vmin=-1.,vmax=1.)
+	cbar = plt.colorbar(heatmap)
+
+	# Set ticks in center of cells
+	ax.set_xticks(np.arange(corr.shape[1]) + 0.5, minor=False)
+	ax.set_yticks(np.arange(corr.shape[0]) + 0.5, minor=False)
+
+	# Rotate the xlabels
+	ax.set_xticklabels(var_names,rotation=45,fontsize=14)
+	ax.set_yticklabels(var_names,fontsize=14)
+
+	plt.xlabel('MEAN',fontsize=16)
+	plt.ylabel('VAR',rotation=90,fontsize=16)
+
+	plt.tight_layout()
+	plt.show()
+	
+#==================================================
+
+
+
+
+
+
+
+
+
+
+
+
