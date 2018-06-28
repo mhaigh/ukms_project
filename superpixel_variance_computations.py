6516cf8f6cf2f708cc0cb7cbcd1de0ba6c303642
Matt Garrod <mg1115@ic.ac.uk>
2018-06-28 17:11:06 +0100

    update to segmentation varianve

--- superpixel_variance_computations.py
+++ superpixel_variance_computations.py
@@ -71,5 +71,71 @@ def Get_Super_Pixel_STDs(variable,segments) :
     return STD_Vals , Variance_Vals 
 
 
+def Put_into_segmentation_matrix(segments,STD_Vals) : 
+
+    """
+	
+	Convert to matrix.
+    
+    """
+
+
+
+    All_STD_Vals = np.zeros((segments.shape[0],segments.shape[1]))
+    
+
+    for i in range(segments.shape[0]-1) : 
+        #print(i)
+        for j in range(segments.shape[1]-1) : 
+            try : 
+                seg_val = segments[i][j] -1
+                All_STD_Vals[i][j] = STD_Vals[seg_val]
+            except : 
+                print("segment = " + str(segments[i][j]))
+                seg_val = segments[i][j] -1
+                print("len std vals = " + str(len(STD_Vals)))
+                print(STD_Vals[seg_val])
+                
+    return All_STD_Vals
+                
+
+
+def Plot_on_Segments(Scalar_Field,Variable_Name,filename='plot') :
+
+	"""
+	Plot some field associated with the different segments.
+
+	"""
+
+	#Load a single instance of the data set so that we can get the latitude and longitude coordinates:
+
+	f = mogreps.download_data('mogreps-uk', mogreps.make_data_object_name('mogreps-uk',2013,2,1,3,0,3),data_folder=Path('.')  )
+	data_set = netCDF4.Dataset(f)
+
+
+	rotation = data_set['rotated_latitude_longitude']
+	transform=ccrs.RotatedPole(pole_longitude=rotation.grid_north_pole_longitude,pole_latitude=rotation.grid_north_pole_latitude)
+	projection = transform
+
+	fig = plt.figure(figsize=(20,10))
+	#create an axis instance:
+	ax = fig.add_subplot(111,projection=projection)
+	pcm = ax.pcolormesh(data_set['grid_longitude'],data_set['grid_latitude'],Scalar_Field,transform=transform,cmap='jet')
+	ax.coastlines(resolution='10m')
+	#ax.colorbar()
+	colbar = fig.colorbar(pcm)
+	#ax.imshow(mark_boundaries(image, segments))
+	#colbar.set_label("VAR("  + str(variable_name) + ")")
+	plt.title(Variable_Name)
+	plt.savefig(filename, bbox_inches='tight' ) 
+	plt.show()
+
+
+
+	
+	
+	
+	
+	
 
 #STD_Vals ,Variance_Vals  = Get_Super_Pixel_STDs('air_temperature',segments)
