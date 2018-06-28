02982cd0d366e8fcf4cf45403199674704ed3071
Matt Garrod <mg1115@ic.ac.uk>
2018-06-28 15:56:56 +0100

    added try except statement to account for different variable depth

--- superpixel_variance_computations.py
+++ superpixel_variance_computations.py
@@ -11,7 +11,14 @@ def Get_Super_Pixel_STDs(variable,segments) :
     Given a segmentation into superpixels we compute the variance
     in a given variable across ensemble members for each superpixel.
     
-    We then return the variance across the
+    We then return the variance across the different ensemble members.
+    
+    Parameters
+    ------------
+    
+    variable : str
+    
+    String specifying the property we are interested in analyisng 
     
     """
 
@@ -32,7 +39,14 @@ def Get_Super_Pixel_STDs(variable,segments) :
         for i in range(num_ensemble_members) : 
             f = mogreps.download_data('mogreps-uk', mogreps.make_data_object_name('mogreps-uk',2013,2,1,3,i,3),data_folder=Path('.')  )
             data_set = netCDF4.Dataset(f)
-            average_for_this_ensemble_member_in_this_super_pixel = np.mean( data_set.variables[variable][0][0].data[segments==PIXEL]) 
+            
+            #Try except fudge to account for the fact that different arrays are at different depths:
+            try : 
+                average_for_this_ensemble_member_in_this_super_pixel = np.mean( data_set.variables[variable][0].data[segments==PIXEL] )
+            except : 
+                average_for_this_ensemble_member_in_this_super_pixel = np.mean( data_set.variables[variable][0][0].data[segments==PIXEL] )
+            
+            
             Averages_in_this_pixel.append(average_for_this_ensemble_member_in_this_super_pixel)
 
         #compute the varaiance for this superpixel:
@@ -46,5 +60,4 @@ def Get_Super_Pixel_STDs(variable,segments) :
 
 
 
-
-STD_Vals ,Variance_Vals  = Get_Super_Pixel_STDs('air_temperature',segments)
+#STD_Vals ,Variance_Vals  = Get_Super_Pixel_STDs('air_temperature',segments)
